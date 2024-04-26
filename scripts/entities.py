eff95045fb5399ad29d9cd5fe98c5cc6407cacb7
MERAB TODUA <merabtodua7@gmail.com>
2024-04-26 14:13:05 +0200

    adding running, jumping, idle, animations

--- scripts/entities.py
+++ scripts/entities.py
@@ -4,14 +4,34 @@ class PhysicsEntity:
 
     def __init__(self, game, e_type, pos, size) -> None:
         self.game = game
-        self.e_type = e_type
+        self.type = e_type
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+
+        self.action = ''
+        self.anim_offset = (-3, -3)#for to overflow animation movement form player rect
+        self.flip = False
+        self.set_action('idle')
+
         
 
     def rect(self):
-        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])     
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])  
+
+    
+    
+    
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
+          
+      
+
+
+     
 
     def update(self, tilemap, movement=(0, 0)):
         
@@ -50,13 +70,42 @@ class PhysicsEntity:
                     self.collisions['up'] = True
                 self.pos[1] = entity_rect.y  
 
+        
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0 :
+            self.flip = True    
+
+
         self.velocity[1] = min(5, self.velocity[1] + 0.1) #Terminal speed
         
         #making sure that after reaching ground speed of fooling is goes to its original values "0"
         if self.collisions['down'] or self.collisions['up']:
-            self.velocity[1] = 0          
+            self.velocity[1] = 0 
+
+        self.animation.update()   
         
 
     def render(self, surf, offset=(0, 0)):
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1]-offset[1] + self.anim_offset[1]))  
 
-        surf.blit(self.game.assets['player'], (self.pos[0]-offset[0], self.pos[1]-offset[1]))    
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size) -> None:
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0
+    
+    def update(self, tilemap, movement = (0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+
+        if self.collisions['down']:
+            self.air_time = 0
+        if self.air_time > 4:
+            self.set_action('jump') 
+        elif movement[0] != 0 :
+            self.set_action('run')
+        else:
+            self.set_action('idle')    
+
--- scripts/utils.py
+++ scripts/utils.py
@@ -16,3 +16,25 @@ def load_images(path):
 
     return images    
 
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True) -> None:
+        self.images = images
+        self.loop = loop
+        self.img_duration = img_dur
+        self.done = False
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.img_duration, self.loop)
+
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+        else:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
+                 self.done = True    
+
+    def img(self):
+        return self.images[int(self.frame / self.img_duration)]  
